1e7ee8ac9fe05a4a2d7ff489cf701996aa3d8517
YicongLuo <91440619+YicongLuo@users.noreply.github.com>
2021-11-21 21:03:19 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -1,11 +1,24 @@
-# Add our dependencies.
+
+# The-data-we-need-to-retrieve
+    # Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+# 1.The total number of votes cast
+    # Add our dependencies.
 import csv
 import os
-# Assign a variable to load a file from a path.
+    # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Open the election results and read the file.
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
@@ -13,11 +26,47 @@ with open(file_to_load) as election_data:
     headers = next(file_reader)
 
     # Print each row in the CSV file.
-    print(headers)
-# The-data-we-need-to-retrieve
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
 
-# 1.The total number of votes cast
+    # 3. Print the total votes.
+print(total_votes)
 # 2 A complete list of candidates who-received-votes
+    # Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Open the election results and read the file.
+# 1. Initialize a total vote counter.
+total_votes = 0
+# Candidate Options
+candidate_options = []
+# Declare the empty dictionary.
+candidate_votes = {}
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    # Read the header row.
+    headers = next(file_reader)
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+       # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
 # 3 The percentage of votes each candidate won
+
 # 4 The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
